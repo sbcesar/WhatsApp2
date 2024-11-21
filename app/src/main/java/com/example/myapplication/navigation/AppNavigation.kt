6053e27e93c50ce02000086b5b8902fe4c1d75c6
sbcesar <148891359+sbcesar@users.noreply.github.com>
2024-11-21 23:51:10 +0100

    he quitado el comentario

--- app/src/main/java/com/example/myapplication/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/myapplication/navigation/AppNavigation.kt
@@ -1,14 +1,11 @@
 package com.example.myapplication.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.myapplication.screens.FirstScreen
 import com.example.myapplication.screens.SecondScreen
-import com.example.myapplication.screens.WhatsAppApi
 
 @Composable
 fun AppNavigation() {
